cdd3d06c44a9da91696d91855ea66f5049a594d2
GuillermoKessler <guillermokessler@gmail.com>
2022-05-13 16:42:56 -0300

    feat: init commit

---
+++ src/main/kotlin/io/kess/rediskeyspacenotifications/RediskeyspacenotificationsApplication.kt
@@ -0,0 +1,11 @@
+package io.kess.rediskeyspacenotifications
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RediskeyspacenotificationsApplication
+
+fun main(args: Array<String>) {
+	runApplication<RediskeyspacenotificationsApplication>(*args)
+}
---
+++ src/test/kotlin/io/kess/rediskeyspacenotifications/RediskeyspacenotificationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.kess.rediskeyspacenotifications
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RediskeyspacenotificationsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
